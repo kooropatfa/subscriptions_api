7507a162770d9f08d2b419a0bdb599dddcefcd8b
Marek Lotkowski <1493261+kooropatfa@users.noreply.github.com>
2021-11-20 16:28:41 +0100

    Migration: create available products in database

--- db/migrate/20211116232235_create_available_products.rb
+++ db/migrate/20211116232235_create_available_products.rb
@@ -3,9 +3,13 @@ class CreateAvailableProducts < ActiveRecord::Migration[6.1]
     Product.create(name: 'Bronze Box', price: 1999, billing_interval: 1)
     Product.create(name: 'Silver Box', price: 4900, billing_interval: 1)
     Product.create(name: 'Gold Box', price: 9900, billing_interval: 1)
+  
+    p 'Products created.'
   end
 
   def down
     Product.destroy_all
+
+    p 'Products deleted.'
   end
 end
